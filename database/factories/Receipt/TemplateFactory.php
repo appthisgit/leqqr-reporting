58d7bd50b772fcabc53133434252bc7d99b3ddef
Jelle D. J. Smits <j.smits@eenprogrammeur.nl>
2023-08-21 11:07:26 +0000

    Removed plugin

---
+++ database/factories/Receipt/TemplateFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Template>
+ */
+class TemplateFactory extends Factory
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model|TModel>
+     */
+    protected $model = \App\Models\Receipt\Template::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
