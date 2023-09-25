905fdd8c134ea76321066decd0e74a9d4dd6dc38
Jelle D. J. Smits <j.smits@eenprogrammeur.nl>
2023-09-25 15:06:11 +0000

    Fixes

--- app/Helpers/Paddings.php
+++ app/Helpers/Paddings.php
@@ -4,4 +4,13 @@ namespace App\Helpers;
 
 class Paddings extends Margins
 {
+    public function copy(): Paddings
+    {
+        return new Paddings(
+            $this->top,
+            $this->right,
+            $this->bottom,
+            $this->left
+        );
+    }
 }
--- app/Helpers/PrintSettings.php
+++ app/Helpers/PrintSettings.php
@@ -4,17 +4,23 @@ namespace App\Helpers;
 
 class PrintSettings
 {
-    public array $endChars;
+    public Margins $lineMargins;
+    public Paddings $paddings;
 
-    public Margins $lineMargins = new Margins(2,0,0,0);
     public string $font = 'Lucida Console';
     public int $fontSize = 9;
     public bool $bold = false;
     public bool $center = false;
 
-    public Paddings $paddings = new Paddings(0,0,0,0);
+
     public int $widthPaper = 217;
     public int $widthCharAmount = 46;
 
     public bool $copyrightFooter = true;
+
+    public function __construct()
+    {
+        $this->lineMargins =  new Margins(2, 0, 0, 0);
+        $this->paddings = new Paddings(0, 0, 0, 0);
+    }
 }
