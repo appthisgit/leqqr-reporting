9e7a003ba561086bdecef6ad6102c4cee36af251
Jelle D. J. Smits <j.smits@eenprogrammeur.nl>
2023-10-17 09:57:19 +0000

    Changed url to /

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
             )
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('/')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
