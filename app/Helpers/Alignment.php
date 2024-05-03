1f3f86bc6bad39a35494fe833bf4051d964c811a
Jelle D. J. Smits <j.smits@eenprogrammeur.nl>
2024-05-03 11:34:17 +0000

    Can't load defined values in a const in php-fpm

--- app/Helpers/Alignment.php
+++ app/Helpers/Alignment.php
@@ -4,9 +4,7 @@ namespace App\Helpers;
 
 enum Alignment: int
 {
-    case left = STR_PAD_RIGHT;
-    case center = STR_PAD_BOTH;
-    case right = STR_PAD_LEFT;
-
-
+    case left = 1;   // STR_PAD_RIGHT
+    case center = 2; // STR_PAD_BOTH
+    case right = 0;  // STR_PAD_LEFT
 }
